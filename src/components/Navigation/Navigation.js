1d78aa794c2292febd6a9795bfa5bb12c2016bef
Evgeniy Taranov <je.taranov@yandex.ru>
2023-10-26 03:04:25 +0300

    Правки после 1го ревью

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,13 +5,13 @@ function Navigation({ closeBurgerMenu }) {
   return (
     <section className="burger-menu">
       <div className="burger-menu__container">
-        <button type="button" onClick={closeBurgerMenu} className="burger-menu__close-button"></button>
+        <button type="button" onClick={closeBurgerMenu} className="burger-menu__close"></button>
         <nav className="burger-menu__menu">
           <NavLink onClick={closeBurgerMenu} to="/" className={({ isActive }) => `${isActive ? "burger-menu__link burger-menu__link_active" : "burger-menu__link"}`} >Главная</NavLink>
           <NavLink onClick={closeBurgerMenu} to="/movies" className={({ isActive }) => `${isActive ? "burger-menu__link burger-menu__link_active" : "burger-menu__link"}`}>Фильмы</NavLink>
           <NavLink onClick={closeBurgerMenu} to="/saved-movies" className={({ isActive }) => `${isActive ? "burger-menu__link burger-menu__link_active" : "burger-menu__link"}`}>Сохраненные фильмы</NavLink>
         </nav>
-        <NavLink onClick={closeBurgerMenu} to="/profile" className="burger-menu__link-account">Аккаунт</NavLink>
+        <NavLink onClick={closeBurgerMenu} to="/profile" className="burger-menu__link-profile">Аккаунт</NavLink>
       </div>
     </section>
   );
